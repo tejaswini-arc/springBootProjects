50bd470294eea45faba9b6f9bc3093a7f5536842
Tejaswini <tejaswiinipawar02@gmail.com>
2026-06-12 21:00:31 +0530

    product controller added

--- src/main/java/com/example/securityExample/controller/ProductController.java
+++ src/main/java/com/example/securityExample/controller/ProductController.java
@@ -1,4 +1,12 @@
 package com.example.securityExample.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ProductController {
+   @GetMapping("/hello")
+    public String hello(){
+        return "HelloWorld";
+    }
 }
